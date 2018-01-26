738ee77147027aebd23af221fba2d72ed4ddd172
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-01-25 18:09:46 -0800

    reinstantiate objects from local storage and render them to table under form

--- app_cart.js
+++ app_cart.js
@@ -1,2 +1,64 @@
 'use strict';
 
+// ** array of all product instances **
+var lSArray = [];
+lSArray = JSON.parse(localStorage.getItem('shoppingCart'));
+console.log(lSArray);
+
+Product.allProducts = [];
+
+var shoppingCartTable = document.getElementById('table-chart');
+
+
+// constructor for products
+function Product(filepath, name, quantityInCart) {
+  this.filepath = filepath;
+  this.name = name;
+  this.quantityInCart = quantityInCart;
+  // this.timesShown = timesShown;
+  // this.timesClicked = timesClicked;
+  Product.allProducts.push(this);
+  // arrayOfProductNames.push(this.name);
+}
+
+Product.prototype.render = function () {
+  console.log('hit render');
+
+  //render image
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.innerHTML = '<img src=' + this.filepath + ' alt="' + this.name + '">';
+  trEl.appendChild(tdEl);
+
+  //render name
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.name;
+  trEl.appendChild(tdEl);
+  //render quantity in cart
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.quantityInCart;
+  trEl.appendChild(tdEl);
+
+  shoppingCartTable.appendChild(trEl);
+};
+
+
+
+
+function makeNewProducts() {
+  console.log('hit makeNewProducts');
+
+  for(var i in lSArray){
+    new Product(lSArray[i].filepath, lSArray[i].name, lSArray[i].quantityInCart);
+  }
+
+  renderTable();
+}
+
+function renderTable() {
+  for(var i in Product.allProducts){
+    Product.allProducts[i].render();
+  }
+}
+
+makeNewProducts();
